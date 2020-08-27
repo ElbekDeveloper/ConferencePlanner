96324248fbd37bd65fc02afd7e23625702d1e40c
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-08-27 19:01:06 +0500

    feat: add speaker controller

--- BackEnd/Startup.cs
+++ BackEnd/Startup.cs
@@ -26,7 +26,7 @@ namespace BackEnd
                 options.UseSqlite("Data Source=conferences.db");
             });
             services.AddControllers();
-        }
+        }  
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
