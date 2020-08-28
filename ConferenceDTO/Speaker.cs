bd4590e8e718d6f947b2662e9bbb56d04e6ab0c0
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-08-28 19:26:34 +0500

    add class library with all the data

---
+++ ConferenceDTO/Speaker.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConferenceDTO
+{
+    public class Speaker
+    {
+        /// <summary>
+        /// Automatically generated speaker id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Name of the Speaker
+        /// </summary>
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+        /// <summary>
+        /// Brief biographical information about the speaker
+        /// </summary>
+        [StringLength(4000)]
+        public string Bio { get; set; }
+        /// <summary>
+        /// The website of the speaker
+        /// </summary>
+        [StringLength(1000)]
+        public virtual string WebSite { get; set; }
+    }
+}
